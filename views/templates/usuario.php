0babc2a1894fc2b200006b08492e7936015a54d6
RiversHIRSCH <61602942+RiversHIRSCH@users.noreply.github.com>
2020-07-25 00:39:05 -0500

    Update usuario.php

--- views/templates/usuario.php
+++ views/templates/usuario.php
@@ -194,41 +194,6 @@ if (isset($_SESSION['user_id'])) {
                                         <span class="sr-only">Next</span>
                                     </a>
                                 </div>
-                                <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-                                    <ol class="carousel-indicators">
-                                        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                                        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-                                    </ol>
-                                    <div v-for="(triptico, index) in tripticos" class="carousel-inner">
-                                        <div v-if="index === 0">
-                                            <div class="carousel-item active">
-                                                <img v-bind:src="'data:image/jpeg;base64,'+triptico.imagen" alt="..." class="d-block w-100" style="height: 70vh;">
-                                                <div class="carousel-caption d-none d-md-block">
-                                                    <h5 class="text-white">{{triptico.nombre}}</h5>
-                                                    <p class="text-white">{{triptico.descripcion}}</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div v-else>
-                                            <div class="carousel-item">
-                                                <img v-bind:src="'data:image/jpeg;base64,'triptico.imagen" alt="..." class="d-block w-100" style="height: 70vh;">
-                                                <div class="carousel-caption d-none d-md-block">
-                                                    <h5 class="text-white">{{triptico.nombre}}</h5>
-                                                    <p class="text-white">{{triptico.descripcion}}</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                        <span class="sr-only">Previous</span>
-                                    </a>
-                                    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                        <span class="sr-only">Next</span>
-                                    </a>
-                                </div>
                             </div>
                         </div>
                         <div class="tab-pane fade" id="videos" role="tabpanel" aria-labelledby="videos-tab">
